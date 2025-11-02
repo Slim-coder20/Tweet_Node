93ec4f1c3002545f715437bae6a584899a495661
slim <slimabida21@gmail.com>
2025-11-02 23:27:46 +0100

    Amélioration du formulaire de tweet avec action POST et indentation corrigée

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 // Import du module express.Router pour créer un routeur
 const router = require("express").Router();
 
-
-// Définition de la route sur la page d'acceuil 
+// Définition de la route sur la page d'acceuil
 router.get("/", (req, res) => {
   res.render("tweets/tweet-list");
-})
+});
 module.exports = router;
+
+// Définition de la route sur la page de création de tweet//
+router.get("/tweet/new", (req, res) => {
+ res.render("tweets/tweet-form");
+});
