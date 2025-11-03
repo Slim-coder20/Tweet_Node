2451173d3cb19bb2aeddefdcd473563b551ab3e8
slim <slimabida21@gmail.com>
2025-11-03 01:21:59 +0100

    Mise à jour des routes et de la liste des tweets

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 // Import du module express.Router pour créer un routeur
 const router = require("express").Router();
 const api = require("./api");
-
+// Import du module tweet.model pour la création du modèle
+const Tweet = require("../database/models/tweet.model");
 
 // connecter API tweets à la route /api/tweets
 router.use("/api", api);
 
 // Définition de la route sur la page d'acceuil
 router.get("/", (req, res) => {
-  res.render("tweets/tweet-list");
+  Tweet.find({})
+    .exec()
+    .then((tweets) => {
+      res.render("tweets/tweet-list", { tweets });
+    });
 });
 
 // Définition de la route sur la page de création de tweet//
